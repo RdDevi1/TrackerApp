5d739642848278b3a33308e8987cb772bdf50fd6
Vitaly <anpilov.vitaly@gmail.com>
2023-11-23 22:23:19 +0300

    add coordinators protocols and extension UINavigationController

---
+++ TrackerApp/Coorditators/CoordinatorProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  CoordinatorProtocol.swift
+//  TrackerApp
+//
+//  Created by Vitaly Anpilov on 23.11.2023.
+//
+
+import Foundation
---
+++ TrackerApp/Helpers/UINavigationController+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  UINavigationController+Ext.swift
+//  TrackerApp
+//
+//  Created by Vitaly Anpilov on 23.11.2023.
+//
+
+import Foundation
